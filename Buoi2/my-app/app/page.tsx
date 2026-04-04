3fb8ad0b9dd297ca167b2ab099341b9f63b4b977
vandat-snk <datlamson2005@gmail.com>
2026-04-04 13:50:58 +0700

    first commit

---
+++ Buoi2/my-app/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="bg-amber-500">Welcome to My App</h1>
+      <h1 className="bg-blue-800 inline-block text-white"> Hello, Next.js!</h1>
+    </div>
+  );
+}
